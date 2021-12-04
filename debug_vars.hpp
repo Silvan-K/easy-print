e3dfce976dd2807d53ab689d0e8795aceb78da59
Silvan-K <silvan_kuttimalai@gmx.de>
2021-12-04 14:16:17 -0800

    Rename functions and introduce namespace

--- debug_vars.hpp
+++ debug_vars.hpp
@@ -3,80 +3,87 @@
 #include <type_traits>
 #include <iostream>
 
-//////////////////////////////////////////////////////////////////////
-// Instantiating these templates with some type T will automatically
-// pick up the one with the correct partial template specialization
-// via SFINAE. I.e. is_iterable<T>::value == true if T is iterable and
-// ids_iterable<T>::value == false if T is not iterable. We consider T
-// iterable if we can call std::begin/end on it.
-//////////////////////////////////////////////////////////////////////
+namespace easyPrint {
 
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
+  //////////////////////////////////////////////////////////////////////
+  // Instantiating these templates with some type T will automatically
+  // pick up the one with the correct partial template specialization
+  // via SFINAE. I.e. is_iterable<T>::value == true if T is iterable and
+  // ids_iterable<T>::value == false if T is not iterable. We consider T
+  // iterable if we can call std::begin/end on it.
+  //////////////////////////////////////////////////////////////////////
 
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-				  decltype(std::end  (std::declval<T>()))>>
-  : std::true_type {};
+  template <typename T, typename = void>
+  struct is_iterable : std::false_type {};
 
-////////////////////////////////////////////////////////////////////
-// Ideally we want two different plain print functions with partial
-// template specialization. One for 'iterable=true' and one for
-// 'iterable=false'. Since partial template specialization is not
-// allowed for functions, use structs instead.
-///////////////////////////////////////////////////////////////////
+  template <typename T>
+  struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+				    decltype(std::end  (std::declval<T>()))>>
+    : std::true_type {};
 
-// Class template definition
-template<typename T, bool = is_iterable<T>::value >
-struct _STREPR_;
+  ////////////////////////////////////////////////////////////////////
+  // Ideally we want two different plain print functions with partial
+  // template specialization. One for 'iterable=true' and one for
+  // 'iterable=false'. Since partial template specialization is not
+  // allowed for functions, use structs instead.
+  ///////////////////////////////////////////////////////////////////
 
-// Partial specialization for iterable=false
-template<typename T>
-struct _STREPR_<T, false> {
-  _STREPR_(std::ostream& strm, const T& arg) {
-    strm << arg;
-  }
-};
+  // Class template definition
+  template<typename T, bool = is_iterable<T>::value >
+  struct _stringRep_;
 
-// Partial specialization for iterable=true
-template<typename T>
-struct _STREPR_<T, true> {
-  _STREPR_(std::ostream& strm, const T& arg) {
-    _STREPR_<std::string, false>(strm, "[");
-    if(std::begin(arg) !=std::end(arg)){
-      for(auto it=std::begin(arg); it!=std::end(arg)-1; ++it) {
-	_STREPR_<decltype(*it)>(strm, *it);
-	_STREPR_<std::string, false>(strm, ", ");
-      }
-      _STREPR_<decltype(*std::end(arg))>(strm, *(std::end(arg)-1));
+  // Partial specialization for iterable=false
+  template<typename T>
+  struct _stringRep_<T, false> {
+    _stringRep_(std::ostream& strm, const T& arg) {
+      strm << arg;
     }
-    _STREPR_<std::string, false>(strm, "]");
+  };
+
+  // Partial specialization for iterable=true
+  template<typename T>
+  struct _stringRep_<T, true> {
+    _stringRep_(std::ostream& strm, const T& arg) {
+      _stringRep_<std::string, false>(strm, "[");
+      if(std::begin(arg) !=std::end(arg)){
+	for(auto it=std::begin(arg); it!=std::end(arg)-1; ++it) {
+	  _stringRep_<decltype(*it)>(strm, *it);
+	  _stringRep_<std::string, false>(strm, ", ");
+	}
+	_stringRep_<decltype(*std::end(arg))>(strm, *(std::end(arg)-1));
+      }
+      _stringRep_<std::string, false>(strm, "]");
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  // Now define a variadic template that allows an arbitrary number of
+  // arguments as the high-level user facing function.
+  //////////////////////////////////////////////////////////////////////
+
+  // End of recursion: end the line
+  void _print_(std::ostream& strm) {
+    strm << std::endl;
   }
-};
 
-//////////////////////////////////////////////////////////////////////
-// Now define a variadic template that allows an arbitrary number of
-// arguments as the high-level user facing function.
-//////////////////////////////////////////////////////////////////////
+  // Recursive expansion of parameter packs
+  template <typename T,
+	    typename... Types>
+  void _print_(std::ostream& strm,
+		    const T& arg,
+		    const Types&... args) {
+    _stringRep_<T, is_iterable<T>::value>(strm, arg);
+    _stringRep_<std::string, false>(strm, " ");
+    _print_(strm, args...);
+  }  
 
-// End of recursion: end the line
-void _DEBUG_VARS_(std::ostream& strm) {
-  strm << std::endl;
+  // Finally the user-facing functions
+  template <typename... Types>
+  void print(const Types&... args) {
+    _print_(std::cout, args...);
+  }
+  template <typename... Types>
+  void printErr(const Types&... args) {
+    _print_(std::cerr, args...);
+  }  
 }
-
-// Recursive expansion of parameter packs
-template <typename T,
-	  typename... Types>
-void _DEBUG_VARS_(std::ostream& strm,
-		  const T& arg,
-		  const Types&... args) {
-  _STREPR_<T, is_iterable<T>::value>(strm, arg);
-  _STREPR_<std::string, false>(strm, " ");
-  _DEBUG_VARS_(strm, args...);
-}  
-
-// Finally the user-facing function
-template <typename... Types>
-void DEBUG_VARS(const Types&... args) {
-  _DEBUG_VARS_(std::cout, args...);
-}  
--- example.cpp
+++ example.cpp
@@ -7,13 +7,16 @@ int main(int argc, char** argv) {
   std::vector<std::vector<int>> nested_vec{std::vector<int>{1,2,3,4},
                                            std::vector<int>{5,6,7}  };
 
-  DEBUG_VARS(12, "<- this is an integer");
+  
+  easyPrint::print(12, "<- this is an integer");
+  
+  easyPrint::printErr(12, "<- this number will end up in stderr");
 
-  DEBUG_VARS("Some vector", std::vector<int>{1,2,3},
-	     "followed by an int", 12,
-	     "and another vector", std::vector<double>(3,12.0),
-	     "and a nested vector", nested_vec,
-	     "and an empty vector", std::vector<double>(),
-	     "an an array", std::array<double, 3>());
-
+  easyPrint::print("Some vector", std::vector<int>{1,2,3},
+		   "followed by an int", 12,
+		   "and another vector", std::vector<double>(3,12.0),
+		   "and a nested vector", nested_vec,
+		   "and an empty vector", std::vector<double>(),
+		   "an an array", std::array<double, 3>());
+  
 }
